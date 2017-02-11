afb569f018ae969fc2e3183553474cfcc6efc483
Nav Aulakh <nav@navaulakh.com>
2017-02-10 23:47:56 -0800

    Tabs to spaces

--- peeps/models.py
+++ peeps/models.py
@@ -6,21 +6,21 @@ db = orm.core.Database()
 
 
 class Users(db.Entity):
-	id = orm.core.PrimaryKey(int, auto=True)
-	uuid = orm.core.Optional(uuid.UUID, default=uuid.uuid4)
-	username = orm.core.Required(str)
-	token = orm.core.Optional(str)
-	access_token = orm.core.Optional(str)
-	refresh_token = orm.core.Optional(str)
-	expiry = orm.core.Optional(datetime.datetime)
+    id = orm.core.PrimaryKey(int, auto=True)
+    uuid = orm.core.Optional(uuid.UUID, default=uuid.uuid4)
+    username = orm.core.Required(str)
+    token = orm.core.Optional(str)
+    access_token = orm.core.Optional(str)
+    refresh_token = orm.core.Optional(str)
+    expiry = orm.core.Optional(datetime.datetime)
 
 
 class Employee(db.Entity):
-	id = orm.core.PrimaryKey(int, auto=True)
-	uuid = orm.core.Optional(uuid.UUID, default=uuid.uuid4)
-	given_name = orm.core.Required(str)
-	family_name = orm.core.Required(str)
-	email = orm.core.Required(str, unique=True)
-	manager = orm.core.Optional("Employee", reverse="manager")
-	role = orm.core.Optional(str)
+    id = orm.core.PrimaryKey(int, auto=True)
+    uuid = orm.core.Optional(uuid.UUID, default=uuid.uuid4)
+    given_name = orm.core.Required(str)
+    family_name = orm.core.Required(str)
+    email = orm.core.Required(str, unique=True)
+    manager = orm.core.Optional("Employee", reverse="manager")
+    role = orm.core.Optional(str)
 
